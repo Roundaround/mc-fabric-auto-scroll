c8c5be1258fe2725bea25a288eed32b071ee4970
Roundaround <roundaroundna@gmail.com>
2025-03-27 14:12:05 -0400

    Update to 1.21.5

--- src/main/java/me/roundaround/autoscroll/client/AutoScrollClientMod.java
+++ src/main/java/me/roundaround/autoscroll/client/AutoScrollClientMod.java
@@ -1,6 +1,7 @@
 package me.roundaround.autoscroll.client;
 
 import me.roundaround.autoscroll.client.event.MinecraftClientEvents;
+import me.roundaround.gradle.api.annotation.Entrypoint;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
@@ -10,16 +11,19 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.player.PlayerInventory;
 
+@Entrypoint(Entrypoint.CLIENT)
 public class AutoScrollClientMod implements ClientModInitializer {
   public static KeyBinding activateKeyBinding;
   public static boolean isScrolling = false;
 
   @Override
   public void onInitializeClient() {
-    activateKeyBinding = KeyBindingHelper.registerKeyBinding(
-        new KeyBinding("autoscroll.key.activate", InputUtil.Type.KEYSYM, InputUtil.UNKNOWN_KEY.getCode(),
-            KeyBinding.MISC_CATEGORY
-        ));
+    activateKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        "autoscroll.key.activate",
+        InputUtil.Type.KEYSYM,
+        InputUtil.UNKNOWN_KEY.getCode(),
+        KeyBinding.MISC_CATEGORY
+    ));
 
     MinecraftClientEvents.ON_INPUT.register((client) -> {
       if (client.currentScreen != null) {
@@ -48,7 +52,11 @@ public class AutoScrollClientMod implements ClientModInitializer {
       }
 
       PlayerInventory inventory = client.player.getInventory();
-      inventory.setSelectedSlot(Scroller.scrollCycling(-1, inventory.selectedSlot, PlayerInventory.getHotbarSize()));
+      inventory.setSelectedSlot(Scroller.scrollCycling(
+          -1,
+          inventory.getSelectedSlot(),
+          PlayerInventory.getHotbarSize()
+      ));
     });
   }
 }
